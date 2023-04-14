ab5cc2a0c7bd6343e6dbc085674dec48d9815ed1
AlvaroOrtizP <alvaro.ortiz.pedroso@gmail.com>
2023-04-15 01:22:58 +0200

    se modifica el codigo para ser mas facil y se añade conversor de los datos a json

--- windWuBot.py
+++ windWuBot.py
@@ -8,6 +8,70 @@ import datetime
 import time
 import re
 import json
+import requests
+import prettytable as pt
+from telegram import ParseMode
+import telegram
+from telegram.ext import CommandHandler, Updater
+
+
+#INICIO BOT 
+
+# Definir token del bot
+TOKEN = "6025769421:AAFxAQwC6nsGsKOYIjCzW5vNaFiounc8tTU"
+
+# Inicializar bot
+bot = telegram.Bot(token=TOKEN)
+
+# Funciones para los comandos
+def saludar(update, context):
+    message = "¡Hola! ¿Cómo estás?"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+def tiempo(update, context):
+    data = json.loads(main())  # json_string es la variable que contiene tu JSON
+    table_data = []
+    for hora, info in data.items():
+        viento = info['viento']
+        rafagas = info['rafagas']
+        altura_olas = info['olas_altura']
+        periodo_olas = info['periodo_olas']
+        temp_tierra = info['temperatura_tierra']
+        table_data.append((hora, viento, rafagas, altura_olas, periodo_olas, temp_tierra))
+    table = pt.PrettyTable(['Hora', 'Viento', 'Ráfagas', 'Altura de olas', 'Periodo de olas', 'Temperatura tierra'], hrules=pt.ALL)
+    table.align['Hora'] = 'l'
+    table.align['Viento'] = 'r'
+    table.align['Ráfagas'] = 'r'
+    table.align['Altura de olas'] = 'r'
+    table.align['Periodo de olas'] = 'r'
+    table.align['Temperatura tierra'] = 'r'
+    table.field_names = table.field_names
+    for row in table_data:
+        table.add_row(row)
+    update.message.reply_text(f'<pre>{table}</pre>', parse_mode=ParseMode.HTML)
+    
+    
+
+    
+    
+
+
+
+# Manejadores de comandos
+saludar_handler = CommandHandler('saludar', saludar)
+tiempo_handler = CommandHandler('tiempo', tiempo)
+
+# Inicializar Updater y agregar manejadores
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(saludar_handler)
+dispatcher.add_handler(tiempo_handler)
+
+# Iniciar bot
+updater.start_polling()
+
+
+#FIN BOT
 
 
 # ID de los elementos HTML que se van a buscar en la página
@@ -19,6 +83,10 @@ DOM_DIRECCION = 'tabid_0_0_SMER'
 DOM_PERIODO_OLAS = 'tabid_0_0_PERPW'
 DOM_DIRECCION_OLAS = ''
 DOM_TEMPERATURA_TIERRA = 'tabid_0_0_TMPE'
+
+
+CANTIDAD_JSON = 20
+
 
 def configurar_navegador():
 # Configura el navegador para ejecutarse en modo headless y establece el tamaño de la ventana
@@ -45,15 +113,24 @@ def encontrar_div(html):
 
 
 def obtener_datos_cabecera(tr):
-# Extrae los datos de la cabecera de la tabla
+    # Extrae los datos de la cabecera de la tabla
     datos = tr.find_all('td')
     resultados = []
+    contador = 0  # Agregar contador
+
     for dato in datos:
+        # Salir del bucle si ya se han obtenido 50 resultados
+        if contador == CANTIDAD_JSON:
+            break
+
         # Extrae el número de punto y la hora a partir del texto de cada celda
         numero_punto = re.findall(r'\d+\.', dato.get_text())[0]
         numero_h = re.findall(r'\d+h', dato.get_text())[0]
         resultados.append(f"{numero_punto} {numero_h}")
+        contador += 1  # Incrementar contador
+
     return resultados
+
 
 
 def guardar_cabecera_en_archivo(tr):
@@ -66,7 +143,7 @@ def guardar_cabecera_en_archivo(tr):
 def obtener_body(tr):
     # Extrae los datos del cuerpo de la tabla
     datos = tr.find_all('td')
-    return [dato.get_text() for i, dato in enumerate(datos) if i < 110]
+    return [dato.get_text() for i, dato in enumerate(datos) if i < CANTIDAD_JSON]
 
 
 
@@ -87,7 +164,7 @@ def crear_json_padre(datos):
         ola_altura = olas_altura_datos[i] 
         periodo_ola = periodo_olas[i] 
         temperatura_tierra = temperaturas_tierra[i]
-        resultado[json.dumps({"hora" : hora})] = {"viento": viento, "rafagas": rafaga, "olas_altura": ola_altura, "periodo_olas": periodo_ola, "temperatura_tierra": temperatura_tierra}
+        resultado[json.dumps(hora)] = {"viento": viento, "rafagas": rafaga, "olas_altura": ola_altura, "periodo_olas": periodo_ola, "temperatura_tierra": temperatura_tierra}
 
     return json.dumps(resultado)
 
@@ -103,7 +180,7 @@ def main():
     tr = div.find('tr', {'id': DOM_CABECERA})
     resultados = obtener_datos_cabecera(tr)    
     
-    guardar_cabecera_en_archivo(div.find('tr', {'id': DOM_DIRECCION}))
+    #guardar_cabecera_en_archivo(div.find('tr', {'id': DOM_DIRECCION}))
     
     datos = []
     datos.append(resultados)
@@ -116,7 +193,8 @@ def main():
     
     print(crear_json_padre(datos))
     driver.quit()
+    return crear_json_padre(datos)
 
-
+"""""
 if __name__ == '__main__':
-    main()
+    main()"""""
