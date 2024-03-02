df72fadc29ec8bb4b7ade88657cb8f21edda6e6d
Alvaro Ortiz Pedroso <alvaro.ortiz.pedroso@gmail.com>
2024-03-02 19:52:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='proyecto_python',
+    name='WebWeatherScraper',
     version='1.0',
     packages=find_packages(),
     entry_points={
@@ -14,6 +14,7 @@ setup(
     },
     install_requires=[
         'requests',
-        'selenium'
+        'selenium',
+        'setuptools',
     ],
 )
