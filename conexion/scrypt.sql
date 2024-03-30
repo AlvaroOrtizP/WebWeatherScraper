a3154e20a51185043080112524608c19ea5edda4
Alvaro Ortiz Pedroso <alvaro.ortiz.pedroso@gmail.com>
2024-03-30 17:39:08 +0100

    Se añaden campos al script

--- conexion/scrypt.sql
+++ conexion/scrypt.sql
@@ -17,8 +17,19 @@ CREATE TABLE `windconditions` (
   `site` varchar(50) NOT NULL,
   `time_of_day` varchar(30) NOT NULL,
   `wind` int(11) DEFAULT NULL,
+  `wind_direction` decimal(5,2) DEFAULT NULL,
   `gusts_of_wind` double DEFAULT NULL,
   `wave_height` varchar(5) NOT NULL,
   `wave_period` int(11) DEFAULT NULL,
   `earth_temperature` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+ALTER TABLE `weatherdata`
+  ADD PRIMARY KEY (`year`,`month`,`day`,`site`);
+
+
+ALTER TABLE `windconditions`
+  ADD PRIMARY KEY (`year`,`month`,`day`,`time_of_day`,`site`);
+COMMIT;
